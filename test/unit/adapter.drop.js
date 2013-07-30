1ea1fe54963c3929b658b7600d27891b8d1de9c9
Matt McFarland <matmcfarland@gmail.com>
2013-07-30 10:15:08 -0500

    add drop unit tests

--- test/unit/adapter.drop.js
+++ test/unit/adapter.drop.js
@@ -11,5 +11,75 @@ var assert = require('assert'),
  */
 
 describe('adapter `.drop()`', function() {
+  before(function(done) {
+    var definition = {
+      id: {
+        type: 'integer',
+        primaryKey: true,
+        autoIncrement: true
+      },
+      email: {
+        type: 'string',
+        unique: true
+      }
+    };
+
+    Support.Setup('drop', definition, function(err) {
+      if(err) throw err;
+
+      Adapter.create('drop', { email: 'jabba@hotmail.com' }, function(err) {
+        if(err) throw err;
+        done();
+      });
+    });
+  });
+
+  it('should create all index sets', function(done) {
+    Adapter._transaction.exec(done, function(callback) {
+      var redis = this;
+
+      redis.exists('waterline:drop:_meta', function(err, exists) {
+        if(err) throw err;
+        assert(exists);
+
+        redis.exists('waterline:drop:id', function(err, exists) {
+          if(err) throw err;
+          assert(exists);
+
+          redis.exists('waterline:drop:email', function(err, exists) {
+            if(err) throw err;
+            assert(exists);
+            callback();
+          });
+        });
+      });
+    });
+  });
+
+  it('should drop all index sets', function(done) {
+    Adapter.drop('drop', function(err) {
+      assert(!err);
+
+      Adapter._transaction.exec(done, function(callback) {
+        var redis = this;
+
+        redis.exists('waterline:drop:_meta', function(err, exists) {
+          if(err) throw err;
+          assert(!exists);
+
+          redis.exists('waterline:drop:id', function(err, exists) {
+            if(err) throw err;
+            assert(!exists);
+
+            redis.exists('waterline:drop:email', function(err, exists) {
+              if(err) throw err;
+              assert(!exists);
+              callback();
+            });
+          });
+        });
+      });
+    });
+  });
 
 });
